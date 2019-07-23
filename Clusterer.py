39232fb256cdd53f80628ddcbe52c412cfc4f195
Anton Lohner Piazza <alohnerpiazza@gmail.com>
2019-07-23 09:41:38 -0500

    Both centroid_lons and centroid_lats are pretty much identical in form to lons and lats, so I have no idea why on earth basemap is doing two wildly different things with both.

--- Clusterer.py
+++ Clusterer.py
@@ -8,6 +8,8 @@ from BaseMap_Plotter import basemap_plotter
 
 lons = []
 lats = []
+centroid_lons = []
+centroid_lats = []
 for i in A["records"]:
     if i['datt_lon'] == "":
         ""
@@ -41,19 +43,19 @@ plt.scatter(df['x'], df['y'], color=colors, alpha=0.5, edgecolor='k')
 for idx, centroid in enumerate(centroids):
     plt.scatter(*centroid, color=colmap[idx+1])
 
-
 plt.ylim([-100.6, -100.1])
 plt.xlim([25.5, 25.9])
 plt.show()
+
 basemap = basemap_plotter()
-i = 0
-# TODO: We can add them to a separate list to ensure they have a special color, if we can't add the colors to the plots.
-#while i < len(lats):
-#    lons.append(df['x'][i])
-#    lats.append(df['y'][i])
-#    i = i + 1
+
+for idx, centroid in enumerate(centroids):
+    centroid_lons.append(float(centroids[0][0].item()))
+    centroid_lats.append(float(centroids[0][1].item()))
+x, y = basemap(centroid_lons, centroid_lats)
+basemap.plot(x, y, markersize=10)
+plt.show()
 x, y = basemap(lons, lats)
 basemap.plot(x, y, 'bo', markersize=2)
-
 # basemap.arcgisimage(service='World_Street_Map', xpixels=2500, verbose=True)
 plt.show()
